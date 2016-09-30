bf35a46d11864cd7e20b7f85680fb49b322762e8
Gabriel Konat <gabrielkonat@gmail.com>
2016-09-30 11:05:31 +0200

    Use virtualenv to run releng script

--- releng/run.sh
+++ releng/run.sh
@@ -4,18 +4,40 @@ set -eu
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if command -v pip3 >/dev/null 2>&1; then
-  pip3 install --user -q -r "$DIR/requirements.txt"
-elif command -v pip >/dev/null 2>&1; then
-  pip install --user -q -r "$DIR/requirements.txt"
-else
-  echo "Cannot find 'pip3' or 'pip' to install required libraries, it should be included with a recent Python 3 installation"
-fi
-
+# Determine Python executable
 if command -v python3 >/dev/null 2>&1; then
-  python3 -u "$DIR/main.py" $*
+  PYTHON_CMD='python3'
 elif command -v python >/dev/null 2>&1; then
-  python -u "$DIR/main.py" $*
+  PYTHON_CMD='python'
 else
   echo "Cannot find 'python3' or 'python' interpreter, please install Python 3"
+  exit 1
 fi
+
+# Determine PIP executable
+if command -v pip3 >/dev/null 2>&1; then
+  PIP_CMD='pip3'
+elif command -v pip >/dev/null 2>&1; then
+  PIP_CMD='pip'
+else
+  echo "Cannot find 'pip3' or 'pip' to install required libraries, it should be included with a recent Python 3 installation"
+  exit 1
+fi
+
+# Create and activate virtualenv
+VENV="$DIR/.virtualenv"
+$PIP_CMD install --quiet --user --upgrade virtualenv
+$PYTHON_CMD -m virtualenv --quiet "$VENV" > /dev/null
+# Since virtualenv is made by idiots, temporarily disable unbound variable checks when activating virtualenv
+set +o nounset
+source "$VENV/bin/activate"
+set -o nounset
+
+# Install requirements
+$PIP_CMD install --quiet --requirement "$DIR/requirements.txt"
+
+# Run script
+$PYTHON_CMD -u "$DIR/main.py" $*
+
+# Deactivate virtualenv
+deactivate
